209ae1921856043c7ce4d1fdfb744e7ed1a143a1
jchuang02 <jchuangsa@gmail.com>
2017-10-27 20:24:38 -0700

    some codewill be added, not finished

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -3,4 +3,8 @@
 import {greet, invertCase} from '../js/app'; //import the functions to test!
 
 /* tests go here */
-
+describe('Checking invertCase', function() {
+  it('should all be uppercase', function() {
+    expect(document.querySelector('d-none')).toEqual(null);
+  });
+});
--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -4,4 +4,8 @@ import * as fs from 'fs'; //import functions for working with file system
 const htmlContent = fs.readFileSync('index.html', 'utf-8'); //load the HTML content
 
 /* Your tests go here */
-
+describe('check for DANGER', function() {
+  it('should show alert', function() {
+    expect(document.querySelector('d-none')).toEqual(null);
+  });
+});
